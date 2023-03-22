34bd0d697ac571efa4ec951663f62c9f7200e7cf
Johannes Møgster <johannes.mogster@mojotech.no>
2023-03-22 20:15:12 +0000

    black

--- src/examplepackage/example_module.py
+++ src/examplepackage/example_module.py
@@ -11,26 +11,23 @@ class Aclass:
     Class docstring. Write something clever.
     Maybe something about inputs and outputs.
     """
-    def __init__(
-            self,
-            first_variable,
-            second_variable=2
-    ):
+
+    def __init__(self, first_variable, second_variable=2):
         self.first_variable = first_variable
         self.second_variable = second_variable
-        _LOGGER.debug('init a class')
+        _LOGGER.debug("init a class")
 
     def add(self):
-        """ This function adds. """
-        _LOGGER.debug('add first and second variable')
+        """This function adds."""
+        _LOGGER.debug("add first and second variable")
         return self.first_variable + self.second_variable
 
     def set_a(self, first_variable):
-        """ This function sets a variable. """
-        _LOGGER.debug('set first variable')
+        """This function sets a variable."""
+        _LOGGER.debug("set first variable")
         self.first_variable = first_variable
 
     def set_b(self, second_variable):
-        """ This function sets another variable. """
-        _LOGGER.debug('set second variable')
+        """This function sets another variable."""
+        _LOGGER.debug("set second variable")
         self.second_variable = second_variable
--- tests/test_example_module.py
+++ tests/test_example_module.py
@@ -10,39 +10,31 @@ class TestAclass(TestCase):
     Class docstring. Write something clever.
     Maybe something about inputs and outputs.
     """
+
     def setUp(self):
-        """ set up for tests. """
+        """set up for tests."""
         self.aclass = Aclass(first_variable=0)
 
     def tearDown(self):
-        """ tear down after tests. """
+        """tear down after tests."""
         return None
 
     def test_set_a(self):
-        """ this is a test. """
+        """this is a test."""
         expected = 2
         self.aclass.set_a(first_variable=expected)
         result = self.aclass.first_variable
-        self.assertEqual(
-            first=expected,
-            second=result
-        )
+        self.assertEqual(first=expected, second=result)
 
     def test_set_b(self):
-        """ this is a test. """
+        """this is a test."""
         expected = 2
         self.aclass.set_b(second_variable=expected)
         result = self.aclass.second_variable
-        self.assertEqual(
-            first=expected,
-            second=result
-        )
+        self.assertEqual(first=expected, second=result)
 
     def test_add(self):
-        """ this is a test. """
+        """this is a test."""
         expected = 2
         result = self.aclass.add()
-        self.assertEqual(
-            first=expected,
-            second=result
-        )
+        self.assertEqual(first=expected, second=result)
